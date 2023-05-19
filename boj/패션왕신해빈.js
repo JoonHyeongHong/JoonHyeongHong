ef23085fe7329ed5ebb0451c19df862cee1eff2b
JoonHyeongHong <jshwa0429@gmail.com>
2023-05-19 16:45:20 +0900

    5월 19일 문제 풀이 #2

---
+++ boj/패션왕신해빈.js
@@ -0,0 +1,33 @@
+/*
+해빈이는 패션에 매우 민감해서 한번 입었던 옷들의 조합을 절대 다시 입지 않는다. 예를 들어 오늘 해빈이가 안경, 코트, 상의, 신발을 입었다면, 다음날은 바지를 추가로 입거나 안경대신 렌즈를 착용하거나 해야한다. 해빈이가 가진 의상들이 주어졌을때 과연 해빈이는 알몸이 아닌 상태로 며칠동안 밖에 돌아다닐 수 있을까?
+*/
+
+const fs = require("fs");
+const input = fs
+  .readFileSync("/home/sesa/JS_Cote/boj/example.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .reverse();
+
+function factorial(n) {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+let testCase = Number(input.pop());
+while (testCase--) {
+  const clothesCount = input.pop();
+  const clothes = {};
+  for (let i = 0; i < clothesCount; i++) {
+    const [_, kind] = input.pop().split(" ");
+    clothes[kind] ??= 0;
+    clothes[kind]++;
+  }
+  const kinds = Object.values(clothes);
+  let days = 1;
+  for (const kind of kinds) {
+    days *= kind + 1;
+  }
+  console.log(days - 1);
+}
